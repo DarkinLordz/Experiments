fa73ae7de2c361dd549a9f87c0d3539e69c54406
Vahid Khalafov <vahid.khalafovv@gmail.com>
2026-03-11 14:16:08 +0400

    Added calculator ahh

---
+++ calculator.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+    if (argc != 4){
+        return 1;
+    }
+
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[3]);
+    
+    char operator = argv[2][0];
+    
+    int result = 0;
+
+    if (operator == '+'){
+        result = num1 + num2;
+    } else if (operator == '-'){
+        result = num1 - num2;
+    } else if (operator == '*'){ 
+        result = num1 * num2;
+    } else if (operator == '/'){
+        if (num2 == 0) {
+            return 1;
+        }
+        result = num1 / num2;
+    } else {
+        return 1;
+    }
+
+    printf("%d %c %d = %d\n", num1, operator, num2, result);
+
+    return 0;
+}
