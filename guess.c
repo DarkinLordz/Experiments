3e4fcbac9feca683760af658421ed0b60fa79631
Vahid Khalafov <vahid.khalafovv@gmail.com>
2026-03-23 13:35:33 +0400

    Added guess.c

---
+++ guess.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char *argv[]){
+    srand(time(NULL));
+
+    int num = (rand() % 10) + 1;
+    int guess;
+
+    if (argc == 2){
+        const char *mode = argv[1];
+        if (strcmp(mode, "debug") == 0){
+            printf("%d\n", num);
+        }
+    }
+
+    while (1){
+        scanf("%d", &guess);
+        if (guess == num){
+            break;
+        } else {
+            continue;
+        }
+    }
+}
