1bf8ff49f94792b84d5d7c9d17171b25e7874440
Vahid Khalafov <vahid.khalafovv@gmail.com>
2026-03-27 12:35:34 +0400

    Another random ahh

--- test.c
+++ test.c
@@ -2,9 +2,8 @@
 
 int main() {
     int arr[] = {10, 20, 30, 40, 50};
-    int n = sizeof(arr) / sizeof(arr[0]);
     
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) { // LMAO
         printf("%d\n", arr[i]);
     }
     
